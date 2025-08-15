94c2fd48c82f507340d970fb0fc0cbbfb0c8d2d4
Benjamin Leggett <benjamin@edera.io>
2025-08-15 13:46:33 -0400

    Make xargo handle `latest`

--- docker/xargo.sh
+++ docker/xargo.sh
@@ -15,8 +15,9 @@ main() {
     curl --retry 3 -sSfL https://sh.rustup.rs -o rustup-init.sh
     # xargo does not always build with the most recent default version of rust,
     # and it may be desirable to explicitly select a `rust` version anyway,
-    # so allow specifying that
-    if [[ $# -gt 0 ]] && [[ -n "${1}" ]]; then
+    # so allow specifying that. If unspecified or set to `latest`, just use
+    # the rustup default.
+    if [[ $# -gt 0 ]] && [[ -n "${1}" ]] && [[ "${1}" != "latest" ]]; then
         sh rustup-init.sh -y --no-modify-path --profile minimal --default-toolchain="${1}"
     else
         sh rustup-init.sh -y --no-modify-path --profile minimal
